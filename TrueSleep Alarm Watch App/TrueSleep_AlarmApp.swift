bc7ba5eb04f88483f1f20c6d9bab41100860f0d1
energy-management-ua <jam10@ualberta.ca>
2023-07-22 15:51:45 -0600

    Initial Commit

---
+++ TrueSleep Alarm Watch App/TrueSleep_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueSleep_AlarmApp.swift
+//  TrueSleep Alarm Watch App
+//
+//  Created by James McKinnon on 2023-07-22.
+//
+
+import SwiftUI
+
+@main
+struct TrueSleep_Alarm_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
